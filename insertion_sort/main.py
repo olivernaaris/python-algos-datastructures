8daf1e48333dd15bab9e24204e75de739dbdd263
oliver.naaris <oliver.naaris@planetway.com>
2020-12-30 17:36:10 +0200

    Insertion sort implementation.

---
+++ insertion_sort/main.py
@@ -0,0 +1,21 @@
+# Insertion sort
+# Algorithm time complexity: O(n2)
+
+def insertion_sort(arr):
+    """
+    Insertion sort starts from index 1 and compares current index and previous index size.
+    In case value in current index is smaller, we movie it back and compare with previous values until it's sorted.
+    Once previous index values are sorted, we move the primary index forward.
+    The secondary index is used to move back and sort previous values.
+    """
+    for key in range(1, len(arr)):
+        if arr[key] < arr[key-1]:
+            j = key
+            while j > 0 and arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                j -= 1
+
+
+l = [6,1,8,4,10]
+insertion_sort(l)
+print(l)
