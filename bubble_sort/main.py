d0142b26b5c6df4aba0b4eea6e61e599c8a47397
oliver.naaris <oliver.naaris@planetway.com>
2020-12-30 15:36:25 +0200

    Bubble and selection sort implementation

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -1,11 +1,24 @@
+# Bubble sort
+# Algorithm time complexity: O(n2)
+
 def bubble_sort(arr):
-    for num in range(len(arr)-1):
-        if arr[num] > arr[num+1]:
-            print("Swap happened")
-            arr[num], arr[num+1] = arr[num+1], arr[num]
-            print(arr)
+    """
+    Loops trough array elements and changes the position of array elements
+    if left array element is bigger than the right one.
+    Also, add 'swap_happened' variable to keep looping until no additional
+    incorrectly sorted elements exist.
+    """
+    swap_happened = True
+    while swap_happened:
+        print("Bubble sort status: " + str(arr))
+        swap_happened = False
+        for num in range(len(arr)-1):
+            if arr[num] > arr[num+1]:
+                swap_happened = True
+                arr[num], arr[num+1] = arr[num+1], arr[num]
 
 
-l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5] # original case
+# l = [10, 9, 8, 8, 7, 6, 5, 4, 3, 2, 1] # worst case
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10] # best case
 bubble_sort(l)
-
---
+++ selection_sort/main.py
@@ -0,0 +1,23 @@
+# Selection sort
+# Algorithm time complexity: O(n2)
+
+def selection_sort(arr):
+    """
+    Selection sort algorithm to sort a list of numbers using a marker that moves forward
+    after each shift.
+
+    """
+    spot_marker = 0
+    while spot_marker < len(arr):
+        for num in range(spot_marker, len(arr)):
+            if arr[num] < arr[spot_marker]:
+                arr[spot_marker], arr[num] = arr[num], arr[spot_marker]
+        spot_marker += 1
+        print(arr)
+
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5] # original case
+# l = [10, 9, 8, 8, 7, 6, 5, 4, 3, 2, 1] # worst case
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10] # best case
+selection_sort(l)
+print(l)
